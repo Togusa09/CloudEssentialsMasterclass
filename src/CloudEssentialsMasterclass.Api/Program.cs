b4fa8f549f7c81f874cb6e48e42f2e72efe7ff0e
Ben Hockley <ben.hockley@purple.telstra.com>
2022-12-19 12:49:28 +1000

    Allowed cross origin for reactjs app in dev

--- src/CloudEssentialsMasterclass.Api/Program.cs
+++ src/CloudEssentialsMasterclass.Api/Program.cs
@@ -6,7 +6,15 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors(builder => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins("http://localhost:3000", "http://localhost:5000"));
+}
+else
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
