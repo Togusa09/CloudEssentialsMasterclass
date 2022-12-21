a9f1043594723c22ab7a22c0ee012c2952f4fc2e
Tim Marwick <triyuga@gmail.com>
2022-12-21 16:33:04 +1000

    Update - convert blazor app to web api

--- src/CloudEssentialsMasterclass.Api/Program.cs
+++ src/CloudEssentialsMasterclass.Api/Program.cs
@@ -1,35 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddCors();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseCors(builder => builder
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowCredentials()
-        .WithOrigins("http://localhost:3000", "http://localhost:5000"));
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
-app.UseRouting();
-
 app.UseAuthorization();
 
+app.MapControllers();
 
 app.Run();
