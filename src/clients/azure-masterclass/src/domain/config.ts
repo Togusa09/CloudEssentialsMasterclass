99cd20cf72d7d1490eed4f46f75a4bf77d459890
Tim Marwick <triyuga@gmail.com>
2023-01-11 19:05:44 +1000

    Update: aprUrl handling in different environments

--- src/clients/azure-masterclass/src/domain/config.ts
+++ src/clients/azure-masterclass/src/domain/config.ts
@@ -1,7 +1,12 @@
 interface AppConfig {
     apiUrl: string
-  }
-  
-  export const config: AppConfig = (window as any).appConfig as AppConfig || {
-    apiUrl: '',
-  }
+}
+
+console.log("process.env.NODE_ENV", process.env.NODE_ENV)
+
+// See: process.env.NODE_ENV in https://create-react-app.dev/docs/adding-custom-environment-variables/
+const isDev = process.env.NODE_ENV === 'development'
+
+export const config: AppConfig = (window as any).appConfig as AppConfig || {
+    apiUrl: isDev ? 'https://localhost:5001' : '',
+}
